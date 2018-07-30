ecce7f7e99d18df9d90e8172518278e17c69e007
kcscoder <chenchusekharkotari@yandex.com>
2018-07-30 22:46:32 +0530

    First Commit

---
+++ test.R
@@ -0,0 +1,88 @@
+1+1
+2+2
+
+1*4+8
+
+4*5+9
+4*(5+9)
+Pemdas
+
+
+a <- 2
+a
+a <- b <- 5
+a
+b
+
+
+assign('c', 4)
+c
+
+
+d = 8
+d
+
+
+rm('d')
+d
+
+
+R is Case Sensitive
+
+
+A
+a
+
+
+
+Data Types
+
+a <- 2
+a
+class(a)
+
+
+b<- 4
+b
+
+
+c <- 4L
+class(c)
+
+class(5L/2L)
+
+
+x <- "R Prog"
+x
+class(x)
+
+
+is.numeric(4)
+is.integer(5L)
+is.character("Data")
+
+
+
+nchar(x)
+nchar("dvbbvdka")
+nchar(12452)
+
+
+date1 = as.Date("2018-7-25")
+date1
+class(date1)
+as.numeric(date1)
+# from UNIX birth
+
+?as.POSIXct
+17737*24*3600
+date2 <- as.POSIXct("2018-07-25 00:00")
+as.numeric(date2)
+# in Seconds
+
+
+a <- TRUE
+a
+class(a)
+as.logical(5)
+5*5
